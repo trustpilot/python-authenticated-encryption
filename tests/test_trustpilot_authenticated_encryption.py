e4e5c24788948f479bd9e4dc9a7475583044a4bd
Andreas Sion <asi@trustpilot.com>
2017-03-15 08:25:02 +0100

    Simplify tests and CI. For now the "tests" are very simple so I prefer using the build in unittest and not using tox.

--- tests/test_trustpilot_authenticated_encryption.py
+++
@@ -1,9 +0,0 @@
-from trustpilot_authenticated_encryption import encrypt
-
-def test():
-    message = 'this is the secret message'
-    encrypt_key = "g9hH6MkVnlKlGa5IG+5R/uKgyrCJxOsh5fXlwK0mjH0="
-    hash_key = "oGmd/bHHkd+N6P6lZQxyfikjU7c5P/mhWO/noCsERyY="
-    encrypted = encrypt(message.encode("utf-8"), encrypt_key, hash_key)
-
-    assert len(encrypted.decode("ascii")) == 108
